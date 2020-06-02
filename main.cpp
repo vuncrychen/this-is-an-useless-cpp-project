de2e208b838f0ee1684883a9b23376a8d3089771
chiash5732 <chiash5732@gmail.com>
2020-06-02 21:03:39 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+void GuessNumberGame();
+int PrintStar(int);
+void Calc();
+
+int main()
+{
+    int user_input;
+    cout << "你想要玩甚麼遊戲? (1)猜數字 (2)印星號 (3)計算機" << endl;
+    cin >> user_input;
+    switch (user_input)
+    {
+    case 1:
+        GuessNumberGame();
+        break;
+    case 2:
+        
+        PrintStar()
+    
+    default:
+        break;
+    }
+}
+
+void GuessNumberGame()
+{
+
+}
+int PrintStar(int size)
+{
+
+}
+void Calc(){
+
+}
