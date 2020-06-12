077245f7263146d1ccdf4f1497d9e067f108d0bc
vuncrychen <vuncrychen2002@gmail.com>
2020-06-12 22:56:40 +0800

    game complete
    complete the game

--- main.cpp
+++ main.cpp
@@ -1,38 +1,84 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-void GuessNumberGame();
-int PrintStar(int);
-void Calc();
+void PrintVectorThing(string, vector<int>);
+int GuessGameHints(vector<int>, vector<int>);
 
 int main()
 {
-    int user_input;
-    cout << "你想要玩甚麼遊戲? (1)猜數字 (2)印星號 (3)計算機" << endl;
-    cin >> user_input;
-    switch (user_input)
+  vector<int> answer;
+  int guess_count = 0;
+  int game_stat = 1;
+
+  for (int i = 0; i < 4; i ++)
+  {
+    int temp_ans;
+    const char order_ans[4][10] = {
+      "first", "second", "third", "last"
+    };
+    cout << "enter the " << order_ans[i] << " answer: ";
+    cin >> temp_ans;
+    answer.push_back(temp_ans);
+  }
+
+  // PrintVectorThing("answer", answer);
+
+  cout << endl;
+
+  while (game_stat)
+  {
+    vector<int> guess(4);
+    cout << "guess_" << guess_count << ": ";
+    char temp_gue[4];
+    cin >> temp_gue;
+    for (int i = 0; i <= 3; i ++)
     {
-    case 1:
-        GuessNumberGame();
-        break;
-    case 2:
-        
-        PrintStar()
-    
-    default:
-        break;
+      guess[i] = temp_gue[i] - '0';
     }
+
+    // PrintVectorThing("guess", guess);
+
+    game_stat = GuessGameHints(answer, guess);
+    guess_count += 1;
+  }
+
+  return 0;
 }
 
-void GuessNumberGame()
+void PrintVectorThing(string title, vector<int> print_target)
 {
-
+  cout << title << " is: ";
+  for (int i = 0; i < print_target.size(); i ++)
+  {
+    cout << print_target.at(i);
+  }
+  cout << endl;
 }
-int PrintStar(int size)
+
+int GuessGameHints(vector<int> ans, vector<int> gue)
 {
-
-}
-void Calc(){
-
+  int a = 0, b = 0;
+  for (int j = 0; j < gue.size(); j ++)
+  {
+    for (int k = 0; k < gue.size(); k ++)
+    {
+      if (ans[j] == gue[k])
+      {
+        if (ans[j] == gue[j])
+        {
+          a += 1;
+          break;
+        }
+        b += 1;
+      }
+    }
+  }
+  cout << a << "A" << b << "B" << endl;
+  if (a != 4)
+    return 1;
+  else
+    cout << "Congratulations!" << endl;
+    return 0;
 }
